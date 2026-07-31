3c1c4c9576e60ab77ebc42012d2e2459c3089bfb
406-23-noraphon <54513@yupparaj.ac.th>
2026-07-31 10:56:03 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.markdown("# : red[🏃แอปพลิเคชันคำนวณค่าดัชนีมวลกาย BMI]")
+st.markdown("# :red[🏃แอปพลิเคชันคำนวณค่าดัชนีมวลกาย BMI]")
 st.write("กรอกข้อมูลน้ำหนักและส่วนสูงของคุณ เพื่อเช็กสุขภาพเบื้องต้น")
 
-weight = st.number_input("กรอกน้ำหนักของคุณ (กิโลกรัม) : ")
-height_cm = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร) : ")
+weight = st.number_input("กรอกน้ำหนักของคุณ (กิโลกรัม) :")
+height_cm = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร) :")
 
 if st.button("คำนวณค่า BMI 🦄"):
 
